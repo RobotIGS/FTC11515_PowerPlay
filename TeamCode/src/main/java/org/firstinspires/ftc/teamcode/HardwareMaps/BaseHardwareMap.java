3f68cac79982c0becbaf224c0c1ad23937158303
RobotIGS CAD <robot.igs@igs-goettingen.de>
2022-09-29 16:02:17 +0200

    clean up base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMaps/BaseHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMaps/BaseHardwareMap.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.HardwareMaps;
 
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public abstract class BaseHardwareMap {
@@ -12,15 +10,6 @@ public abstract class BaseHardwareMap {
     public DcMotor motor_front_left;
     public DcMotor motor_rear_right;
     public DcMotor motor_rear_left;
-    public DcMotor motor_shovel;
-    public DcMotor motor_lift;
-    public DcMotor motor_carousel;
-    public DistanceSensor distanceSensor_front_mid;
-    public DistanceSensor distanceSensor_right;
-    public DistanceSensor distanceSensor_left;
-    public DistanceSensor distanceSensor_carousel;
-    public ColorSensor colorSensor_down;
-
 
     public BaseHardwareMap(HardwareMap hwMap) {
         this.hwMap = hwMap;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/BaseTeleOp.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
-import org.firstinspires.ftc.teamcode.Tools.OmniWheel;
 
 public abstract class BaseTeleOp extends OpMode {
     @Override
