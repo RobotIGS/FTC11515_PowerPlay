90511b5890c35b40e3a48be9c506b125cf10900c
RobotIGS CAD <robot.igs@igs-goettingen.de>
2022-10-11 17:37:14 +0200

    add ServoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/ServoTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.FullHardwareMap;
+
+@TeleOp
+public class ServoTest extends BaseTeleOp {
+    BaseHardwareMap robot;
+    @Override
+    public void initialize() {
+        robot = new FullHardwareMap(hardwareMap);
+    }
+    @Override
+    public void loop() {
+        if (gamepad1.x) {
+            robot.servo1.setPosition(0.5);
+            robot.servo2.setPosition(-0.5);
+        }
+        if (gamepad1.y) {
+            robot.servo1.setPosition(0.0);
+            robot.servo2.setPosition(0.0);
+        }
+    }
+}
+
