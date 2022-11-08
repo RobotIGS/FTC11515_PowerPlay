77ea549bcd283f2a0767879eacd1f435c6183085
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2022-11-08 17:45:00 +0100

    add positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BaseAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BaseAutonomous.java
@@ -3,8 +3,15 @@ package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.FullHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.GyroHardwareMap;
+import org.firstinspires.ftc.teamcode.Tools.FieldNavigation;
 
 public abstract class BaseAutonomous extends LinearOpMode {
+    private BaseHardwareMap robot;
+    private GyroHardwareMap gyro;
+    private FieldNavigation navi;
+
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
@@ -13,12 +20,16 @@ public abstract class BaseAutonomous extends LinearOpMode {
     }
 
     void initialize() {
+        robot = new FullHardwareMap(hardwareMap);
+        navi = new FieldNavigation(robot,gyro,0.0,0.0,0.0,0.,0.0);
     }
 
     public void detectSignal() {
     };
 
     public void driveToJunction() {
+        navi.drive_to_pos(0.0,147.0,0.2,1.0);
+        navi.drive_to_pos(0.0,90.0,0.2,1.0);
 
         /*
         [-108,0,30] (starting position)
@@ -38,7 +49,9 @@ public abstract class BaseAutonomous extends LinearOpMode {
     };
 
     public void driveToZone() {
-
+        navi.drive_to_pos(-29.0,90.0,0.2,1.0);
+        navi.drive_to_pos(-88.0,90.0,0.2,1.0);
+        navi.drive_to_pos(-149.0,90.0,0.2,1.0);
     };
 
     public abstract BaseHardwareMap initializeHardwareMap();
