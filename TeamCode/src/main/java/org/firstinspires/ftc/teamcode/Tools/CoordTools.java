f12471d5b385f9677d8b253ea303e7bf38c20fc4
root <theodor.groh@igs-goettingen.de>
2022-12-02 16:42:15 -0500

    add CoordTools skelet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tools/CoordTools.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Tools;
+
+public class CoordTools {
+    /**
+     * convert robot coords to field coords
+     * @param x x value (robot)
+     * @param z z value (robot)
+     * @return [x,z] (field)
+     */
+    public static double[] Conv_Robot2Field(double x, double z) {
+        double[] ret = new double[2];
+        // TODO: convert
+        return ret;
+    }
+
+    /**
+     * convert field coords to robot coords
+     * @param x x value (field)
+     * @param z z value (field)
+     * @return [x,z] (robot)
+     */
+    public static double[] Conv_Field2Robot(double x, double z) {
+        double[] ret = new double[2];
+        // TODO: convert
+        return ret;
+    }
+}
