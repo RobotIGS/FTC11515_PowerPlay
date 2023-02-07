4814658087bedd0e863895903b09b64d984599d0
root <theodor.groh@igs-goettingen.de>
2023-02-07 09:40:33 -0500

    add new FullControl

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/FullControlNew.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.FullHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.GyroHardwareMap;
+import org.firstinspires.ftc.teamcode.Tools.FieldNavigation;
+
+@TeleOp
+public class FullControlNew extends BaseTeleOp {
+    BaseHardwareMap robot;
+    FieldNavigation navi;
+    GyroHardwareMap gyro;
+
+    public double lift_start_encoder_value;
+
+    @Override
+    public void initialize() {
+        robot = new FullHardwareMap(hardwareMap);
+        gyro = new GyroHardwareMap(hardwareMap);
+        navi = new FieldNavigation(robot, gyro, 0.0, 0.0, 0.0, 0.0/180, 0.0);
+        lift_start_encoder_value  = robot.motor_lift.getCurrentPosition();
+    }
+
+    @Override
+    public void loop() {
+    }
+
+}
