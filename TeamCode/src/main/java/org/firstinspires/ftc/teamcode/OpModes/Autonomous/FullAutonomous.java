3d029c92dfa0e6dc13828a2e8123d1306f235ae5
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2023-03-02 15:18:17 +0100

    Auto stash before merge of "opencv" and "origin/opencv"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/FullAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/FullAutonomous.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
 
+import java.util.Date;
+
 public class FullAutonomous extends BaseAutonomous {
     @Override
     protected void initialize() {
@@ -13,8 +15,16 @@ public class FullAutonomous extends BaseAutonomous {
     @Override
     public void run() {
         detectSignalCV();
+        detectSignal
+        telemetry.addData("signal_detected :", signal_detected);
+        telemetry.update();
+        long start_time = (new Date()).getTime();
+        while (start_time+5000 > (new Date()).getTime()) {
+        }
+        /*
         driveToJunctionHigh();
         driveToZone();
         navi.drive_setMotors(0,0,0,0);
+         */
     }
 };
